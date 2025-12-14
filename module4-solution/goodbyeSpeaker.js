ece5d6df9467c290d16e736a5765a8c86e88e3cb
Gayathri M <gayathrimnair.2005@gmail.com>
2025-12-14 20:58:18 +0530

    Add files via upload

---
+++ module4-solution/goodbyeSpeaker.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var goodbyeSpeaker = {};
+  goodbyeSpeaker.speakWord = "Goodbye";
+
+  goodbyeSpeaker.speak = function (name) {
+    console.log(goodbyeSpeaker.speakWord + " " + name);
+  };
+
+  window.goodbyeSpeaker = goodbyeSpeaker;
+})(window);
